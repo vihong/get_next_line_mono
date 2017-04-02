ae529adba973af855ecdc017b25e45648c990d6a
Vi reangsy HONG <vi-hong@e2r11p2.42.fr>
2017-04-02 18:18:26 +0200

    .h modified

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: vi-hong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 17:01:53 by vi-hong           #+#    #+#             */
-/*   Updated: 2017/04/02 18:15:38 by vi-hong          ###   ########.fr       */
+/*   Updated: 2017/04/02 18:16:49 by vi-hong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 0
+# define BUFF_SIZE 64
 # include <sys/types.h>
 # include <stdlib.h>
 # include <unistd.h>
